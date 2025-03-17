6191ee2707193c640864c9c7bf050f30450aa1d7
dinesh1st <dinesh1st@yahoo.com>
2025-03-18 10:27:31 +1100

    Update pfu_claculator.py

--- pfu_claculator.py
+++ pfu_claculator.py
@@ -5,11 +5,7 @@ st.components.v1.html("""
 <html>
 <head>
     <meta name="google-site-verification" content="Z8q1_ESZgMZb-T9fVWY6Y9TDKDyFqIqF7Sbxn_64b2U" />
-    <meta name="description" content="CFU Calculator - Calculate CFU/mL with dilution and volume inputs. Designed for microbiologists!">
-    <meta name="keywords" content="CFU, microbiology, scientific calculator, dilution factor">
-    <meta name="author" content="Dinesh Subedi">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>CFU Calculator</title>
+    <title>https://cfucalculator.streamlit.app/ homepage</title>
 </head>
 </html>
 """, height=0)
